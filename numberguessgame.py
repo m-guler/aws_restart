f38afcf47e872bcad17c360f456d1a31b48ecec4
Mustafa GULER <44267136+m-guler@users.noreply.github.com>
2022-11-24 11:54:44 +0000

    Update numberguessgame.py

--- numberguessgame.py
+++ numberguessgame.py
@@ -2,9 +2,16 @@
 import random
 import time
 
-print("hi welcome to guessing game plaesae choose number between 1 to 100")
-print("im picking number hmmm..")
-time.sleep(3)
+print("hi welcome to guessing game plaese choose a number between 1 to 100")
+print("im picking a number hmmm.")
+time.sleep(1)
+print("im picking a number hmmm..")
+time.sleep(1)
+print("im picking a number hmmm...")
+time.sleep(1)
+print("Right, I've picked a number, lets guess it")
+time.sleep(2)
+
 guess = int(input("what is ur guess : "))
 
 
