884aed810e0ed867fa38a45444ddef81fb91dd7b
Mustafa GULER <44267136+m-guler@users.noreply.github.com>
2022-11-24 11:27:50 +0000

    Add files via upload

---
+++ pythonlab1.py
@@ -0,0 +1,36 @@
+#name = input("whats ur name  ")
+#color = input("what is ur fav color ")
+#animal = input("what is ur fa animal  ")
+#print("{}, you like {} {}!" .format(name,color,animal))
+
+#myfruitlis = ["apple", "banana", "cherry"]
+#print(myfruitlis)
+#print(type(myfruitlis))
+#print(myfruitlis[0])
+#myfruitlis[2] = "orange"
+#print(myfruitlis)
+
+#myFinalAnswerTuple = ("apple", "banana", "pineapple")
+#print(myFinalAnswerTuple)
+#print(type(myFinalAnswerTuple))
+#print(myFinalAnswerTuple[0])
+#print(myFinalAnswerTuple[2])
+
+#myfavouritefruitdictionary = {
+#    "akua" : "apple",
+#    "saanvi" : "banana",
+#    "paulo" : "pineapple"
+    
+# }
+#print(myfavouritefruitdictionary)
+#print(type(myfavouritefruitdictionary))
+
+#print(myfavouritefruitdictionary["akua"])
+#print(myfavouritefruitdictionary["saanvi"])
+#print(myfavouritefruitdictionary["paulo"])
+
+#myMixedTypeList = [45, 290578, 1.02, True, "My dog is on the bed.", "45"]
+
+#for item in myMixedTypeList:
+ #   print("{} is of the data type {}".format(item,type(item)))
+ 
